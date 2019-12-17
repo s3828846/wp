575f2a36e76a5b0d245b6865184bf95fd3b050b1
s3828846 <s3828846@csitprdap01.int.its.rmit.edu.au>
2019-12-17 18:21:56 +1100

    Forgot to Push (16.12)

--- a2/index.php
+++ a2/index.php
@@ -14,7 +14,7 @@
   <body>
 
     <header>
-      <div>Put company logo and name here</div>
+      <div>Lunardo</div>
     </header>
 
     <nav>
@@ -31,7 +31,7 @@
     <footer>
       <div>&copy;<script>
         document.write(new Date().getFullYear());
-      </script> Put your name(s), student number(s) and group name here. Last modified <?= date ("Y F d  H:i", filemtime($_SERVER['SCRIPT_FILENAME'])); ?>.</div>
+      </script> Oliver Dunn Last modified <?= date ("Y F d  H:i", filemtime($_SERVER['SCRIPT_FILENAME'])); ?>.</div>
       <div>Disclaimer: This website is not a real website and is being developed as part of a School of Science Web Programming course at RMIT University in Melbourne, Australia.</div>
       <div><button id='toggleWireframeCSS' onclick='toggleWireframe()'>Toggle Wireframe CSS</button></div>
     </footer>
